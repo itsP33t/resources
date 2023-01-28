78190e9af207329573a4e74bc9d174d9c61d3ff4
Peter <79012293+itsP33t@users.noreply.github.com>
2023-01-28 22:41:16 +0100

    init 🌟

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+// imports
+import { MantineProvider, AppShell, Navbar, Header } from '@mantine/core';
+
+
+ 
+
+
+// components
+import {HeaderMegaMenu} from './components/navbar';
+
+
+export default function App() {
+  return (
+    <MantineProvider theme={{
+      colorScheme: "dark",
+      colors: {
+        primary: [
+          "#9debfb",
+          "#84e7fa",
+          "#6ce2f9",
+          "#53ddf9",
+          "#09cef6",
+          "#08badd",
+          "#079cba",
+          "#0690ac",
+          "#067c93",
+          "#045262"
+        ],
+        gradientFrom: [
+          "#99e5ff",
+          "#80dfff",
+          "#66d8ff",
+          "#4dd2ff",
+          "#00bfff",
+          "#00abe6",
+          "#0098cc",
+          "#0085b3",
+          "#007299",
+          "#004c66"
+        ],
+        gradientTo: [
+          "#9dd7fb",
+          "#84cdfa",
+          "#6cc3f9",
+          "#53b9f8",
+          "#0a9bf5",
+          "#098fe3",
+          "#087cc4",
+          "#076cac",
+          "#065d93",
+          "#043e62"
+        ]
+      },
+      primaryShade: 6,
+      primaryColor: "primary",
+      defaultRadius: "sm",
+      defaultGradient: {
+        deg: 45,
+        to: "gradientTo",
+        from: "gradientFrom"
+      },
+      fontFamily: "'Poppins', sans-serif",
+      other: {
+        customFontFamily: "'Poppins', sans-serif"
+      },
+      components: {
+        Button: {
+          styles: {
+            root: {
+              borderWidth: 2
+            }
+          }
+        },
+        Chip: {
+          styles: {
+            label: {
+              borderWidth: 2
+            }
+          }
+        },
+        Input: {
+          styles: {
+            input: {
+              borderWidth: 2
+            }
+          }
+        },
+        Pagination: {
+          styles: {
+            item: {
+              borderWidth: 2
+            }
+          }
+        },
+        Switch: {
+          styles: {
+            track: {
+              borderWidth: 2
+            }
+          }
+        }
+      }
+    }} withGlobalStyles withNormalizeCSS >
+      <AppShell
+      padding="md"
+      header={<Header height={5} p="xs">{<HeaderMegaMenu></HeaderMegaMenu>}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      
+    </AppShell>
+    </MantineProvider>
+  );
+}
